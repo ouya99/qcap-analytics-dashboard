993132f64f026a7126f351fddde3d2901776ff29
Neuron99 <Neuron99@mail.com>
2025-05-24 08:36:19 +0400

    fix: useclient

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import VotingDashboard from '@/components/dashboard/voting-dashboard';
 import AllocationDashboard from '@/components/dashboard/allocation-dashboard';
